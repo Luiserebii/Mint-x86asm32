4deff8ac65772be0ea94820c11ac11e6849691c5
Luiserebii <luis@serebii.io>
2020-03-10 15:56:41 -0400

    Continuing to flesh out, seems we'll need a few functions here. Also, would be good to establish the format to print beforehand

--- src/test.s
+++ src/test.s
@@ -11,10 +11,16 @@ indent:
 	.ascii "  \0"
 
 success:
-	.ascii "SUCCESS\0"
+	.ascii "[SUCCESS]\0"
 
 fail:
-	.ascii "FAIL\0"
+	.ascii "[FAIL]\0"
+
+fail_info_init:
+	.ascii ": \0"
+
+endl:
+	.ascii "\n\0"
 
 .section .text
 
@@ -29,6 +35,7 @@ fail:
 # non-zero is true, zero is false.
 #
 .globl test_assert
+.type test_assert, @function
 test_assert:
 	pushl %ebp
 	movl %esp, %ebp
@@ -36,20 +43,31 @@ test_assert:
 	# Concat indent to buffer, and title
 	pushl indent
 	pushl buffer
-	call strcat
+	call _strcat
 
 	movl 8(%ebp), %eax
-	movl %eax, 4(%esp)
-	call strcat
+	movl %eax, -4(%ebp)
+	call _strcat
 
 	# Branch depending on cond
 	cmpl $0, 12(%ebp)
 	je test_assert_if_false
 
 	# Logic for true
+	movl $success, -4(%esp)
+	call _strcat
+	jmp test_assert_if_end
 
 test_assert_if_false:
 	# Logic for false
+	movl $fail, -4(%esp)
+	call _strcat
+
+	movl $fail_info_init, -4(%esp)
+	call_strcat
+	
+	# itoa(cond, buffer, 10)
+	# concat is false at the end
 
 test_assert_if_end:
 
